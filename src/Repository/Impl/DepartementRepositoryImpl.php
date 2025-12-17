8bf0f7b75f721311bf70299a31a5d9911ae2e74d
DIEY178 <dramand619@gmail.com>
2025-12-17 21:00:14 +0000

    creation du cours 1

--- src/Repository/Impl/DepartementRepositoryImpl.php
+++ src/Repository/Impl/DepartementRepositoryImpl.php
@@ -1,18 +1,20 @@
 <?php
 namespace App\Repository\Impl;
-use App\Repository\DepartementRepository;
-use App\Entity\Departement;
-class DepartementRepositoryImpl implements DepartementRepository
+use App\Repository\DepartmentRepository;
+use App\Entity\Department;
+
+class DepartmentRepositoryImpl implements DepartmentRepository
 {
     private array $departements = [];
-     public function insert(Departement $departement): bool{
-        array_push($this->departements, $departement);
+
+    public function insert(Department $department): bool{
+        array_push($this->departements, $department);
         return true;
     }
 
     public function selectAll(): array
     {
-        // Implementation code here
+        
         return $this->departements;
     }
 }
